2f8f51a27142d3227f165905641215c27ad362c8
OrlySpivak <orlyspivak90@gmail.com>
2019-08-24 20:41:08 +0300

    First commit

---
+++ RecipesProj/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RecipesProj.Startup))]
+namespace RecipesProj
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
