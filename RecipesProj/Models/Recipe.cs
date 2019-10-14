0de773cd2976e6f4809c83ac00f7e0f5c0c5cb2b
Nir <gilhod>
2019-10-14 17:48:36 +0300

    FoodType Recipe Ingredient

---
+++ RecipesProj/Models/Recipe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RecipesProj.Models
+{
+    public class Recipe
+    {
+        public int ID { get; set; }
+        public string VideoURL { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime InsertDate { get; set; }
+        public int FoodTypeID { get; set; }
+        public virtual FoodType FoodType { get; set; }
+        public virtual ICollection<Ingredient> Ingredients { get; set; }
+    }
+}
